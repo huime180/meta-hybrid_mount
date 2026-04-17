8a1de2093a42e8facdaa377334c23ecbdd961301
huime <116278145+huime180@users.noreply.github.com>
2026-04-17 13:40:35 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ use mimalloc::MiMalloc;
 static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() -> Result<()> {
-    if std::env::var("KSU_LATE_LOAD").is_ok() && std::env::var("KSU").is_ok() {
-        panic!("! unsupported late load mode");
-    }
     let cli = Cli::parse();
     core::entry::run(cli)
 }
