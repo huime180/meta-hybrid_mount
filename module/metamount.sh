130e65ae52a6b2834fc6eb394845eb2a4616db63
huime <116278145+huime180@users.noreply.github.com>
2026-08-23 00:40:43 +0800

    Implement checks for existing mount files
    Add checks for mounted files before proceeding.

--- module/metamount.sh
+++ module/metamount.sh
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+[! -f "/dev/.esred"] && exit 0
+[ -f "/dev/.mouted"] && exit 0
+touch /dev/.mounted
 MODDIR="${0%/*}"
 BASE_DIR="/data/adb/hybrid-mount"
 RUN_DIR="$BASE_DIR/run"
